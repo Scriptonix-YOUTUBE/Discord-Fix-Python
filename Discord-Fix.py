903397f5a35171d8cfdc6934e777a7d0b3d4d540
Scriptonix-YOUTUBE <89107446+Scriptonix-YOUTUBE@users.noreply.github.com>
2021-10-28 10:11:34 -0500

    Create Discord-Fix.py

---
+++ Discord-Fix.py
@@ -0,0 +1,58 @@
+import os 
+
+import time
+
+
+print('Made by Scriptonix')
+
+
+def split(f): # Checks if the path is the correct path
+    try:
+        t = f.split(chr(92))
+        l = t[len(t)-1]
+        if l.find('app-')>-1:
+            return f
+        else:
+            return False
+    except Exception:
+        return False
+
+def getpath(path): # Gets the path where Discord is located
+    folders = list(os.walk(path))
+    for f in folders:
+        for f2 in f:
+            spl = split(f2)
+            if spl:
+                return spl + '\Discord.exe'
+
+def startdiscord(): # Attempts to start Discord automatically
+    try:
+        path = getpath(os.path.expanduser("~") + "\AppData\Local\Discord")
+        os.startfile(path)
+        print('[SUCCESS]: Attempt to start Discord')
+    except Exception:
+        print('[FAILED]: Attempt to start Discord')
+
+def enddiscord(): # Attempts to end Discord automatically
+    print("Attempting to kill Discord processes")
+    try:
+        os.system("taskkill /im discord.exe /F")
+        return True
+
+    except Exception:
+        return False
+
+
+if enddiscord():
+    print('[SUCCESS]: Attempt to kill Discord processes')
+    time.sleep(.5)
+    print('Attempting to start Discord')
+    startdiscord()
+else:
+    print('[FAILED]: Attempt to kill Discord processes,\nseems like Discord does not have a problem')
+    time.sleep(.5)
+    print('Attempting to start Discord')
+    startdiscord()
+
+
+input("Do not close this or it will cause Discord to close")
